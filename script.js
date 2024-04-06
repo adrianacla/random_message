86a4735ea4610a6a519568446a8f0f404dfa5384
Adriana Lopez <adrianacla1998@gmail.com>
2024-04-06 10:07:52 -0400

    se arregló las oraciones para que tenga sentido

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let listNombre = ["yo", "tu", "el", "si", "universo", "Diosito"];
-let listBonito = ["soy", "eres", "te quiere"];
-let listFin = ["muy pro", "demasiado", "bello", "genial"];
+let listAfirm = ["Eres hermos@", "Eres imporante", "Eres valioso"];
+let listRemember = ["cuidarte a ti mism@", "consentirte y tratare bien", "siempre tratare bien"];
+let listFin = ["Diosito te quiere mucho", "el universo está contigo", "todo te va a salir bien"];
 
 function randomMessage(){
     const listOfIndex = generateIndex();
-    return listNombre[listOfIndex[0]] + " " + listBonito[listOfIndex[1]] + " " + listFin[listOfIndex[2]]
+    return listAfirm[listOfIndex[0]] + ", recuerda " + listRemember[listOfIndex[1]] + ", " + listFin[listOfIndex[2]] + "."
 }
 
 function generateIndex(){
-    let index1= Math.floor(Math.random() * (listNombre.length));
-    let index2= Math.floor(Math.random() * (listBonito.length));
+    let index1= Math.floor(Math.random() * (listAfirm.length));
+    let index2= Math.floor(Math.random() * (listRemember.length));
     let index3= Math.floor(Math.random() * (listFin.length));
 
     return [index1, index2, index3]
